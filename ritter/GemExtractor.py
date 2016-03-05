8fbdef0700c817ce506f4861904bff6f1576d364
johan ahlqvist <Aquila@johans-MacBook-Pro.local>
2016-03-05 23:11:01 +0100

    feat: add gem extractor.

---
+++ ritter/GemExtractor.py
@@ -0,0 +1,33 @@
+import re
+
+class GemExtractor:
+
+    def sentence_reg(tokens):
+        reg = '([^.?!]*(?:' + '|'.join(tokens) + ')[^.?!]*)'
+        return re.compile(reg, re.IGNORECASE)
+
+    def capture_reg(pattern):
+        return re.compile(pattern, re.IGNORECASE)
+
+    def extract_gem(text, gem, artifact):
+        results = []
+        if gem == None:
+            return None
+
+        reg_sentences = GemExtractor.sentence_reg(artifact['tokens'])
+
+        sentences = reg_sentences.findall(text)
+        if len(sentences) == 0:
+            return None
+
+        for pattern in gem['patterns']:
+            reg_capture = GemExtractor.capture_reg(pattern)
+            for sentence in sentences:
+                match = reg_capture.findall(sentence)
+                if len(match) != 0:
+                    results.extend(match)
+
+        if len(results) > 0:
+            return {'name': gem['name'], 'result': results}
+        else:
+            return None
---
+++ tests/test_gemextractor.py
@@ -0,0 +1,21 @@
+import unittest
+
+from ritter.GemExtractor import GemExtractor
+
+class GemExtractorTest(unittest.TestCase):
+
+    def test_sentence_reg(self):
+        sentences = 'Hej mitt namn är Erik, jag är 18 år. Kolla mitt fräcka hår.'
+        tokens = ['erik', '18']
+        result = GemExtractor.sentence_reg(tokens).findall(sentences)
+        self.assertEquals(['Hej mitt namn är Erik, jag är 18 år'], result)
+
+    def test_extract_gem(self):
+        text = 'Hej mitt namn är Erik, jag föddes 18 år gammal. Kolla mitt fräcka hår.'
+        patterns = ['född[\D]*(\d+)', '(\d+)[\D]*född']
+        tokens = ['Erik', '18']
+        gem = {'patterns': patterns, 'name': 'föddes'}
+        artifact = {'tokens': tokens}
+
+        result = GemExtractor.extract_gem(text, gem, artifact)
+        self.assertEquals({'name': 'föddes', 'result': ['18']} , result)
