74b947f2198c1a7a7a98c35dcb5e0bb46e648c16
Erik Gärtner <erik@gartner.io>
2016-04-13 23:51:30 +0200

    feat: Improved artifact structure

--- ritter/analyzerbase.py
+++ ritter/analyzerbase.py
@@ -2,3 +2,17 @@ class AnalyzerBase:
 
     def ritter_id(self):
         return '%s_%s' % (self.ritter_type, self.id)
+
+    def _get_doc(self, collection, _id):
+        collection = self.db[collection]
+        return collection.find_one({'_id': _id})
+
+    def _save_analytics(self, collection, data):
+        collection = self.db['ritterData']
+        collection.remove({'id': self.ritter_id()})
+        doc = {
+            'id': self.ritter_id(),
+            'type': self.ritter_type,
+            'data': data
+        }
+        return collection.insert_one(doc)
--- ritter/artifact_analyzer.py
+++ ritter/artifact_analyzer.py
@@ -8,35 +8,22 @@ class ArtifactAnalyzer(AnalyzerBase):
         self.db = db
         self.ritter_type = 'artifact_analytics'
         self.id = data['id']
+        self.collection = 'artifacts'
 
     def analyze(self):
-        self._get_artifact()
-        if self.artifact is None:
+        artifact = self._get_doc(self.collection, self.id)
+        if artifact is None:
             print('Artifact not found %s' % self.id)
             return
 
         data = {}
-        data.update(self._determine_gender())
+        data.update(self._determine_gender(artifact))
 
-        self._save_analytics(data)
-        print('ArtifactAnalyzer done with %s' % self.artifact['name'])
+        self._save_analytics(self.collection, data)
+        print('ArtifactAnalyzer done with %s' % artifact['name'])
 
-    def _determine_gender(self):
-        full_name = self.artifact['name']
+    def _determine_gender(self, artifact):
+        full_name = artifact['name']
         firstname = full_name.split()[0]
         (gender, probability) = Genderize.guess_from_name(firstname)
         return {'genderize': {'gender': gender}}
-
-    def _get_artifact(self):
-        collection = self.db['artifacts']
-        self.artifact = collection.find_one({'_id': self.id})
-
-    def _save_analytics(self, data):
-        collection = self.db['ritterData']
-        collection.remove({'id': self.ritter_id()})
-        doc = {
-            'id': self.ritter_id(),
-            'type': self.ritter_type,
-            'data': data
-        }
-        collection.insert_one(doc)
