ab2e945549de83b7b42fa87bd2d3846fe8f8b428
Erik Gärtner <erik@gartner.io>
2016-04-14 10:11:09 +0200

    feat: Add markedTree to plaintext

--- ritter/dataprocessors/markdown.py
+++ ritter/dataprocessors/markdown.py
@@ -8,3 +8,10 @@ class Markdown():
         html = markdown(markdown_text)
         text = ''.join(BeautifulSoup(html, 'html.parser').findAll(text=True))
         return text
+
+    def tree_to_plaintext(marked_tree):
+        text_list = []
+        for item in marked_tree:
+            if 'text' in item:
+                text_list.append(item['text'])
+        return '\n'.join(text_list)
