fa566f2a98f318d996f4377d05e7a3438938bcf0
Erik Gärtner <erik@gartner.io>
2016-04-15 13:14:18 +0200

    perf: Improved annotating performance

--- ritter/dataprocessors/annotators.py
+++ ritter/dataprocessors/annotators.py
@@ -4,17 +4,34 @@ import re
 class ArtifactAnnotator:
 
     def linkify_artifacts(marked_tree, artifacts):
+        big_string = ArtifactAnnotator._marked_tree_to_big_string(marked_tree)
+
         for artifact in artifacts:
             link = '(%s "GHOSTDOC-TOKEN")' % artifact['_id']
             for token in artifact['tokens']:
                 reg = ArtifactAnnotator._token_reg(token)
-                for item in marked_tree:
-                    if 'text' in item:
-                        repl = r'[\1]%s' % link
-                        item['text'] = reg.sub(repl, item['text'])
+                repl = r'[\1]%s' % link
+                big_string = reg.sub(repl, big_string)
 
+        ArtifactAnnotator._big_string_to_marked_tree(marked_tree, big_string)
         return marked_tree
 
     def _token_reg(token):
         reg = r'(\b%s)' % token
         return re.compile(reg, re.IGNORECASE)
+
+    def _marked_tree_to_big_string(marked_tree):
+        strings = []
+        for item in marked_tree:
+            if 'text' in item:
+                strings.append(item['text'])
+        big_string = u'\u1394'.join(strings)
+        return big_string
+
+    def _big_string_to_marked_tree(marked_tree, big_string):
+        strings = big_string.split(u'\u1394')
+        i = 0
+        for item in marked_tree:
+            if 'text' in item:
+                item['text'] = strings[i]
+                i = i + 1
