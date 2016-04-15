3e99617ec74a17de4f569fac9b1c631f0852c138
Erik Gärtner <erik@gartner.io>
2016-04-15 10:39:02 +0200

    fix: Too greedy gem extractor due to faulty filtering.

--- ritter/dataprocessors/gem_extractor.py
+++ ritter/dataprocessors/gem_extractor.py
@@ -3,11 +3,17 @@ import re
 
 class GemExtractor:
 
+    _sentence_reg = re.compile(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s')
+
     def extract(tree, artifact, gems):
+        token_reg = GemExtractor._token_reg(artifact['tokens'])
         sentences = []
         for item in tree:
             if 'text' in item:
-                sentences.append(item['text'])
+                sub_sents = GemExtractor._sentence_reg.split(item['text'])
+                for s in sub_sents:
+                    if token_reg.search(s) is not None:
+                        sentences.append(s)
 
         if len(sentences) == 0:
             return []
@@ -37,3 +43,7 @@ class GemExtractor:
 
     def _capture_reg(pattern):
         return re.compile(pattern, re.IGNORECASE)
+
+    def _token_reg(tokens):
+        reg = r'(\b' + r'|\b'.join(tokens) + r')'
+        return re.compile(reg, re.IGNORECASE)
