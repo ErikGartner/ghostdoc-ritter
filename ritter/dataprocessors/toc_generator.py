4ea52814d697bff8ea409c238871367fe0df9163
Erik Gärtner <erik@gartner.io>
2016-04-17 11:32:09 +0200

    fix: Toc Id generation

--- ritter/dataprocessors/toc_generator.py
+++ ritter/dataprocessors/toc_generator.py
@@ -4,7 +4,7 @@ import re
 class TocGenerator:
 
     def generate_toc(marked_tree):
-        regex = re.compile(r'[^A-Za-z]+')
+        regex = re.compile(r'[^A-Za-z0-9_]+')
         toc = []
         for item in marked_tree:
             if item['type'] == 'heading':
