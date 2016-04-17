6656c90bf7f4270be8001720622221dd7ef66e3e
Erik Gärtner <erik@gartner.io>
2016-04-18 00:37:43 +0200

    fix: Hanging headers when last.

--- ritter/dataprocessors/artifact_extractor.py
+++ ritter/dataprocessors/artifact_extractor.py
@@ -33,4 +33,6 @@ class ArtifactExtractor:
             elif mt[i]['type'] == 'list_item_start' and mt[i + 1]['type'] == 'list_item_end':
                 mt[i] = None
                 mt[i + 1] = None
+        if len(mt) > 0 and mt[-1].get('type') == 'heading':
+            mt[-1] = None
         return [i for i in mt if i != None]
