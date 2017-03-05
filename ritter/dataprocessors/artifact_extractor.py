b13ed10fc5837d980baa68da20be02ffe9cf0b49
Erik Gärtner <erik@gartner.io>
2017-03-05 11:48:20 +0100

    fix: List filtering during artifact extraction

--- ritter/dataprocessors/artifact_extractor.py
+++ ritter/dataprocessors/artifact_extractor.py
@@ -10,6 +10,7 @@ class ArtifactExtractor:
                        if ArtifactExtractor._filter_text_items(i, reg)]
 
         marked_tree = ArtifactExtractor._filter_headers(marked_tree)
+        marked_tree = ArtifactExtractor._rec_filter_lists(marked_tree, 0)
         return marked_tree
 
     def _paragraph_reg(tokens):
@@ -30,10 +31,25 @@ class ArtifactExtractor:
                 continue
             if mt[i]['type'] == mt[i + 1]['type'] == 'heading':
                 mt[i] = None
-            elif (mt[i]['type'].endswith('_item_start') and
-                  mt[i + 1]['type'] == 'list_item_end'):
-                mt[i] = None
-                mt[i + 1] = None
         if len(mt) > 0 and mt[-1].get('type') == 'heading':
             mt[-1] = None
         return [i for i in mt if i != None]
+
+    def _rec_filter_lists(mt, i):
+        if i > len(mt) - 1:
+            return mt
+
+        if (mt[i]['type'].endswith('_item_start') and
+            mt[i + 1]['type'] == 'list_item_end'):
+            del mt[i]
+            del mt[i + 1]
+
+        elif (mt[i]['type'].endswith('list_start') and
+            mt[i + 1]['type'] == 'list_end'):
+            del mt[i]
+            del mt[i + 1]
+
+        else:
+            i += 1
+
+        return ArtifactExtractor._rec_filter_lists(mt, i)
